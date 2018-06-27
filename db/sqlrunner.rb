bfeb3bc37839a607dfac6bf8baf6be42a603a1fa
Pete Whittle <peterwhittle75@gmail.com>
2018-06-27 09:59:52 +0100

    final heroku commit

--- db/sqlrunner.rb
+++ db/sqlrunner.rb
@@ -3,7 +3,7 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect({ dbname:'bglibrary', host: 'localhost'})
+      db = PG.connect({ dbname:'bglibrary', host: 'ec2-107-22-174-187.compute-1.amazonaws.com', port: 5432, user: 'ylshkvcwmrfidw', password: '1462d07e49f91b98197741380f894cb9235ac212934fdf763f280adc5073ef6e'})
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
