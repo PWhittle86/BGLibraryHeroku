08388204fe17a32375a1e5e8a915544fcb0c8c3c
Pete Whittle <peterwhittle75@gmail.com>
2018-07-02 11:26:43 +0100

    Heroku troubleshooting 3

--- controllers/games_controller.rb
+++ controllers/games_controller.rb
@@ -2,7 +2,7 @@ require('sinatra')
 require('sinatra/contrib/all') if development?
 require_relative('../models/game')
 require_relative('../models/customer')
-also_reload('../models/*')
+# also_reload('../models/*')
 
 get '/games' do
   @games = Game.all
