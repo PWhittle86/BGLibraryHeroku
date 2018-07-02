bbcaddd2df4a0f31638742302dae287150c79efc
Pete Whittle <peterwhittle75@gmail.com>
2018-07-02 11:38:40 +0100

    Heroku troubleshooting 5

--- db/sqlrunner.rb
+++ db/sqlrunner.rb
@@ -3,7 +3,7 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect({ dbname:'bglibrary', host: 'ec2-107-21-224-61.compute-1.amazonaws.com', port: 5432, user: 'ryummygflecoam', password: 'ee2bcfff35bde480b5812a302fa9c2cf9a04c933ffa86d7bdcafaa26575a0ac6'})
+      db = PG.connect({ dbname:'djrgp70sq272j', host: 'ec2-107-21-224-61.compute-1.amazonaws.com', port: 5432, user: 'ryummygflecoam', password: 'ee2bcfff35bde480b5812a302fa9c2cf9a04c933ffa86d7bdcafaa26575a0ac6'})
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
